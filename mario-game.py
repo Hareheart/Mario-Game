b867924cf907c2eed3d703e254296d75a844c6f9
Hareheart <89731534+Hareheart@users.noreply.github.com>
2022-12-27 12:39:44 +0100

    included actual designs and imported map

--- mario-game.py
+++ mario-game.py
@@ -4,16 +4,20 @@ pygame.font.init()
 
 
 # WINDOW VARIABLES
-WIDTH, HEIGHT = 900, 500
+WIDTH, HEIGHT = 1100, 650
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Mario Game")
+FPS = 60
+
+BACKGROUND_COLOR = "#230c06"
+FIRST_BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join('Assets', 'secondmap.png')), (WIDTH, HEIGHT))
+FINAL_BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join('Assets', 'firstmap.png')), (WIDTH, HEIGHT))
 
 WHITE = 255, 255, 255
 BLACK = 0, 0, 0
 RED = 255, 0, 0
 
-FPS = 60
-
+# FONTS
 health_font = pygame.font.SysFont('comicsans', 40)
 winner_font = pygame.font.SysFont('comicsans', 100)
 
@@ -21,46 +25,49 @@ winner_font = pygame.font.SysFont('comicsans', 100)
 player_hit = pygame.USEREVENT + 1
 
 # SPRITE VARIABLES
-block_image = pygame.image.load(os.path.join('block.png'))
-block_width = 60
-block_height = 60
-block = pygame.transform.scale(block_image, (block_width, block_height))
+warrior_image = pygame.image.load(os.path.join('Assets', 'warrior.png'))
+warrior_width = 60
+warrior_height = 100
 
-ground_width = WIDTH
-ground_height = 10
-ground = pygame.Rect(0, (HEIGHT/2) - (ground_height/2), WIDTH, ground_height)
+enemy_image = pygame.image.load(os.path.join('Assets', 'enemy.png'))
+enemy_width = 60
+enemy_height = 100
 
+bullet_image = pygame.image.load(os.path.join('Assets', 'Bullet.png'))
 bullet_width = 25
 bullet_height = 10
+bullet = pygame.transform.scale(bullet_image, (bullet_width, bullet_height))
 
 # PHYSICS VARIABLES
-gravity = 5
-vel_x = 5
-vel_y = 100
+gravity = 0
+player_vel = 5
 bullet_vel = 8
 max_bullets = 3
 
-
-def draw_window(player, bullets, player_health):
-    screen.fill(WHITE)
-    pygame.draw.rect(screen, BLACK, ground)
+def draw_window(player, enemy, bullets, player_health):
+    screen.fill(BACKGROUND_COLOR)
+    screen.blit(FIRST_BACKGROUND, (0, 0))
 
     health_text = health_font.render("Health: " + str(player_health), 1, BLACK)
     screen.blit(health_text, (10, 10))
 
-    screen.blit(block, (player.x, player.y))
+    screen.blit(warrior_image, (player.x, player.y))
+    screen.blit(enemy_image, (enemy.x, enemy.y))
     for bullet in bullets:
         pygame.draw.rect(screen, RED, bullet)
     pygame.display.update()
 
 
 def physics(keys_pressed, player):
-    if player.y < (HEIGHT/2) - block_height:
-        player.y += gravity
+    global gravity
+    gravity += 0.8
+    player.y += gravity
+
     if keys_pressed[pygame.K_LEFT]:
-        player.x -= vel_x
+        player.x -= player_vel
     if keys_pressed[pygame.K_RIGHT]:
-        player.x += vel_x
+        player.x += player_vel
+
 
 def move_bullets(bullets, player):
     for bullet in bullets:
@@ -76,11 +83,12 @@ def spawn_lose_text(lose_text):
     draw_lose_text = winner_font.render(lose_text, 1, BLACK)
     screen.blit(draw_lose_text, (WIDTH/2 - draw_lose_text.get_width()/2, HEIGHT/2 - draw_lose_text.get_height()/2))
     pygame.display.update()
-    pygame.time.delay(5000)
+    pygame.time.delay(3000)
 
 
 def main():
-    player = pygame.Rect(100, 100, block_width, block_height)
+    player = pygame.Rect(100, 100, warrior_width, warrior_height)
+    enemy = pygame.Rect(700, 100, enemy_width, enemy_height)
 
     bullets = []
 
@@ -93,11 +101,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+                pygame.quit()
 
             if event.type == pygame.KEYDOWN:
                 # Jumping
                 if event.key == pygame.K_UP:
-                    player.y -= vel_y
+                    global gravity
+                    gravity = -10
                 # Shooting
                 if event.key == pygame.K_SPACE and len(bullets) < max_bullets:
                     bullet = pygame.Rect(
@@ -118,9 +128,9 @@ def main():
         keys_pressed = pygame.key.get_pressed()
         physics(keys_pressed, player)
         move_bullets(bullets, player)
-        draw_window(player, bullets, player_health)
+        draw_window(player, enemy, bullets, player_health)
 
-    pygame.quit()
+    main()
 
 
 if __name__ == "__main__":
